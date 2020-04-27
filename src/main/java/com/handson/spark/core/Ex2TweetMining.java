fec22fecdd4d310d7b5fc8f900147a60fafd5078
idob1996 <idob1996@gmail.com>
2020-04-27 15:21:02 +0300

    finish to write the third ex and test

--- src/main/java/com/handson/spark/core/Ex2TweetMining.java
+++ src/main/java/com/handson/spark/core/Ex2TweetMining.java
@@ -1,96 +1,71 @@
 package com.handson.spark.core;
 
-import com.handson.spark.utils.Parse;
-import com.handson.spark.utils.Tweet;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import com.handson.spark.utils.LoadJsonData;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
-import scala.Tuple2;
 
-import java.util.List;
+import java.util.Arrays;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.desc;
 
 /**
- *  The Java Spark API documentation: http://spark.apache.org/docs/latest/api/java/index.html
- *
- *  We still use the dataset with the 8198 reduced tweets. Here an example of a tweet:
- *
- *  {"id":"572692378957430785",
- *    "user":"Srkian_nishu :)",
- *    "text":"@always_nidhi @YouTube no i dnt understand bt i loved of this mve is rocking",
- *    "place":"Orissa",
- *    "country":"India"}
- *
- *  We want to make some computations on the tweets:
- *  - Find all the persons mentioned on tweets
- *  - Count how many times each person is mentioned
- *  - Find the 10 most mentioned persons by descending order
- *
- *  Use the Ex2TweetMiningTest to implement the code.
+ * The Java Spark API documentation: http://spark.apache.org/docs/latest/api/java/index.html
+ * <p>
+ * We still use the dataset with the 8198 reduced tweets. Here an example of a tweet:
+ * <p>
+ * {"id":"572692378957430785",
+ * "user":"Srkian_nishu :)",
+ * "text":"@always_nidhi @YouTube no i dnt understand bt i loved of this mve is rocking",
+ * "place":"Orissa",
+ * "country":"India"}
+ * <p>
+ * We want to make some computations on the tweets:
+ * - Find all the persons mentioned on tweets
+ * - Count how many times each person is mentioned
+ * - Find the 10 most mentioned persons by descending order
+ * <p>
+ * Use the Ex2TweetMiningTest to implement the code.
  */
 public class Ex2TweetMining {
 
-  private static String pathToFile = "data/reduced-tweets.json";
+    private String pathToFile = "data/reduced-tweets.json";
 
-  /**
-   *  Load the data from the json file and return an RDD of Tweet
-   */
-  public JavaRDD<Tweet> loadData() {
-    // create spark configuration and spark context
-    SparkConf conf = new SparkConf()
-        .setAppName("Tweet mining")
-        .set("spark.driver.allowMultipleContexts", "true")
-        .setMaster("local[*]");
 
-    JavaSparkContext sc = new JavaSparkContext(conf);
+    /**
+     * Find all the persons mentioned on tweets (case sensitive)
+     */
+    public Dataset<Row> mentionOnTweet() {
+        Dataset<Row> tweets = LoadJsonData.loadData(pathToFile);
 
-    // load the data and create an RDD of Tweet
-    JavaRDD<Tweet> tweets = sc.textFile(pathToFile)
-                              .map(line -> Parse.parseJsonToTweet(line));
+        Dataset<String> tweetsText = tweets.select(col("text")).as(Encoders.STRING());
+        Dataset<Row> mention = tweetsText.flatMap(tweet -> Arrays.asList(tweet.split(" ")).iterator(), Encoders.STRING())
+                .filter(word -> word.startsWith("@") && word.length() > 1).toDF("mentions");
 
-    return tweets;
-  }
+        return mention;
 
-  /**
-   *  Find all the persons mentioned on tweets (case sensitive)
-   */
-  public Dataset<Row> mentionOnTweet() {
-    JavaRDD<Tweet> tweets = loadData();
+    }
 
-    // You want to return an RDD with the mentions
-    // Hint: think about separating the word in the text field and then find the mentions
-    // TODO write code here
-    JavaRDD<String> mentions = null;
+    /**
+     * Count how many times each person is mentioned
+     */
+    public Dataset<Row> countMentions() {
+        Dataset<Row> mentions = mentionOnTweet();
 
-    return mentions;
+        Dataset<Row> mentionCount = mentions.groupBy(col("mentions")).count();
 
-  }
-  /**
-   *  Count how many times each person is mentioned
-   */
-  public JavaPairRDD<String, Integer> countMentions() {
-    JavaRDD<String> mentions = mentionOnTweet();
+        return mentionCount;
+    }
 
-    // Hint: think about what you did in the wordcount example
-    // TODO write code here
-    JavaPairRDD<String, Integer> mentionCount = null;
+    /**
+     * Find the 10 most mentioned persons by descending order
+     */
+    public Dataset<Row> top10mentions() {
 
-    return mentionCount;
-  }
+        Dataset<Row> mostMentioned = countMentions().sort(desc("count")).limit(10);
 
-  /**
-   *  Find the 10 most mentioned persons by descending order
-   */
-  public List<Tuple2<Integer, String>> top10mentions() {
-    JavaPairRDD<String, Integer> counts = countMentions();
-
-    // Hint: take a look at the sorting and take methods
-    // TODO write code here
-    List<Tuple2<Integer, String>> mostMentioned = null;
-
-    return mostMentioned;
-  }
+        return mostMentioned;
+    }
 
 }
--- src/test/java/com/handson/spark/core/Ex2TweetMiningTest.java
+++ src/test/java/com/handson/spark/core/Ex2TweetMiningTest.java
@@ -1,58 +1,57 @@
 package com.handson.spark.core;
 
 
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import scala.Tuple2;
 
-import java.util.List;
+import static org.apache.spark.sql.functions.col;
 
 public class Ex2TweetMiningTest {
 
-  private Ex2TweetMining ex2TweetMining;
+    private Ex2TweetMining ex2TweetMining;
 
-  @Before
-  public void init() {
-    ex2TweetMining = new Ex2TweetMining();
-  }
+    @Before
+    public void init() {
+        ex2TweetMining = new Ex2TweetMining();
+    }
 
-  @Test
-  public void mentionOnTweet() {
-    // run
-    JavaRDD<String> mentions = ex2TweetMining.mentionOnTweet();
+    @Test
+    public void mentionOnTweet() {
+        // run
+        Dataset<Row> mentions = ex2TweetMining.mentionOnTweet();
 
-    // assert
-    Assert.assertEquals(4462, mentions.count());
-    JavaRDD<String> filter = mentions.filter(mention -> "@JordinSparks".equals(mention));
-    Assert.assertEquals(2, filter.count());
-  }
+        // assert
+        Assert.assertEquals(4462, mentions.count());
+        Dataset<Row> filter = mentions.where(col("mentions").equalTo("@JordinSparks"));
+        Assert.assertEquals(2, filter.count());
+    }
 
-  @Test
-  public void countMentions() {
-    // run
-    JavaPairRDD<String, Integer> counts = ex2TweetMining.countMentions();
+    @Test
+    public void countMentions() {
+        // run
+        Dataset<Row> counts = ex2TweetMining.countMentions();
 
-    // assert
-    Assert.assertEquals(3283, counts.count());
-    JavaPairRDD<String, Integer> filter = counts.filter(couple -> "@JordinSparks".equals(couple._1()));
-    Assert.assertEquals(1, filter.count());
-    Assert.assertEquals(2, filter.first()._2().intValue());
-  }
+        // assert
+        Assert.assertEquals(3283, counts.count());
+        Dataset<Row> filter = counts.where(col("mentions").equalTo("@JordinSparks"));
+        Assert.assertEquals(1, filter.count());
+        Assert.assertEquals(2, filter.first().getLong(1));
+    }
 
-  @Test
-  public void top10mentions() {
-    // run
-    List<Tuple2<Integer, String>> mostMentioned = ex2TweetMining.top10mentions();
+    @Test
+    public void top10mentions() {
+        // run
+        Dataset<Row> mostMentioned = ex2TweetMining.top10mentions();
 
-    // assert
-    Assert.assertEquals(10, mostMentioned.size());
-    Assert.assertEquals(189, mostMentioned.get(0)._1().intValue());
-    Assert.assertEquals("@ShawnMendes", mostMentioned.get(0)._2());
-    Assert.assertEquals(100, mostMentioned.get(1)._1().intValue());
-    Assert.assertEquals("@HIITMANonDECK", mostMentioned.get(1)._2());
-  }
+        // assert
+        Assert.assertEquals(10, mostMentioned.count());
+        Assert.assertEquals(189L, mostMentioned.first().getLong(1));
+        Assert.assertEquals("@ShawnMendes", mostMentioned.first().getString(0));
+        Assert.assertEquals(100L, mostMentioned.collectAsList().get(1).getLong(1));
+        Assert.assertEquals("@HIITMANonDECK", mostMentioned.collectAsList().get(1).getString(0));
+    }
 
 }
